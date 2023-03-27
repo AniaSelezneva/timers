ffea712bbba3706955ae8eb97a5cc6bf38cfd49f
AnnaS <53278654+AniaSelezneva@users.noreply.github.com>
2023-03-27 17:31:32 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div v-for="(timer, index) in timers" :key="index">
+      <TimerComponent :index="index"></TimerComponent>
+    </div>
+    <button @click="addTimer" class="item">
+      <img class="add-icon" src="@/assets/addIcon.svg" alt="Add" />
+    </button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TimerComponent from './components/TimerComponent.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    TimerComponent,
+  },
+  data() {
+    return {
+      timers: [],
+    };
+  },
+  methods: {
+    addTimer() {
+      this.timers.push({ time: 0, running: false, intervalId: null });
+    },
   }
-}
+};
 </script>
 
 <style>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 45px;
+  margin-top: 72px;
+}
+
+.item {
+  width: 225px;
+  height: 120px;
+  background: #696969;
+  color: #9E9E9E;
+  border: none;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.item img {
+  cursor: pointer;
+}
+
+.add-icon:hover {
+    content: url('@/assets/activeAddIcon.svg');
+}
+
+@media screen and (min-width: 768px) {
+  .wrapper {
+    flex-direction: row;
+    flex-wrap: wrap;
+    max-width: 500px;
+    column-gap: 50px;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .wrapper {
+    max-width: 775px;
+  }
+}
+
+body {
+  background: #353638;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
 }
 </style>
---
+++ src/components/TimerComponent.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="item" v-bind:class="{ 'active': stopwatch.running }">
+    <p class="time">{{ formatTime(stopwatch.time) }}</p>
+    <div class="buttons-container">
+      <button @click="stopwatch.running ? pauseTimer() : startTimer()">
+        <img v-if="stopwatch.running" v-bind:src="pauseIconPath" alt="Pause" />
+        <img v-else v-bind:src="playIconPath" alt="Play" />
+      </button>
+      <button @click="stopTimer">
+        <img v-bind:src="stopIconPath" alt="Stop" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import playIcon from '@/assets/playIcon.svg';
+import activePlayIcon from '@/assets/activePlayIcon.svg';
+import pauseIcon from '@/assets/pauseIcon.svg';
+import activePauseIcon from '@/assets/activePauseIcon.svg';
+import stopIcon from '@/assets/stopIcon.svg';
+import activeStopIcon from '@/assets/activeStopIcon.svg';
+
+export default {
+  props: {
+    index: Number,
+  },
+  data() {
+    return {
+      stopwatch: {
+        time: 0,
+        running: false,
+        intervalId: null,
+      },
+    };
+  },
+  methods: {
+    startTimer() {
+      if (!this.stopwatch.running) {
+        this.stopwatch.intervalId = requestAnimationFrame(this.tick);
+        this.stopwatch.running = true;
+      }
+    },
+    pauseTimer() {
+      cancelAnimationFrame(this.stopwatch.intervalId);
+      this.stopwatch.running = false;
+    },
+    stopTimer() {
+      this.pauseTimer();
+      this.stopwatch.time = 0;
+    },
+    formatTime(time) {
+      const hours = Math.floor(time / 3600);
+      const minutes = Math.floor((time % 3600) / 60);
+      const seconds = time % 60;
+
+      let timeString = '';
+      if (hours > 0) {
+        timeString += `${hours}:`;
+      }
+
+      if (minutes > 0 || hours > 0) {
+        timeString += `${minutes.toString().padStart(2, '0')}:`;
+      }
+
+      timeString += `${seconds.toString().padStart(2, '0')}`;
+      
+      return timeString;
+    },
+    tick() {
+      this.stopwatch.time += 1;
+      this.stopwatch.intervalId = requestAnimationFrame(this.tick);
+    },
+  },
+  computed: {
+    playIconPath() {
+      return this.stopwatch.running ? activePlayIcon : playIcon;
+    },
+    pauseIconPath() {
+      return this.stopwatch.running ? activePauseIcon : pauseIcon;
+    },
+    stopIconPath() {
+      return this.stopwatch.running ? activeStopIcon : stopIcon;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.time, .buttons-container {
+  flex: 1;
+  margin: 0;
+  display: flex;
+  align-items: center;
+}
+
+.time {
+    width: 100%;
+    justify-content: center;
+    border-bottom: 1px solid #9E9E9E;
+}
+
+.buttons-container {
+  display: flex;
+  gap: 48px;
+}
+
+.buttons-container button {
+  border: none;
+  padding: 0;
+  background: none;
+  color: #9E9E9E;
+}
+
+.active,
+.active .buttons-container button {
+  color: #FFFFFF;
+}
+.active .time {
+  border-bottom: 1px solid #FFFFFF;
+}
+</style>
+
